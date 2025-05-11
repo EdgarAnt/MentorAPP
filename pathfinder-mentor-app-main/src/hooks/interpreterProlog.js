10cccfa67bef2d8e5f7052ba6394d1c227a0f42e
MiguelGmz7 <oscar.gomez5178@alumnos.udg.mx>
2025-05-10 19:52:56 -0600

    ADD: interpreterProlog.js

---
+++ pathfinder-mentor-app-main/src/hooks/interpreterProlog.js
@@ -0,0 +1,24 @@
+// Import Tau Prolog core and create a session
+const pl = require("tau-prolog");
+const session = pl.create(1000);
+const show = x => console.log(session.format_answer(x));
+
+// Get Node.js argument: node ./script.js item
+const item = process.argv[2];
+
+const goal = `
+    inicializar_puntajes,
+    procesar_respuestas(1,b),
+    carrera_mas_afin(Carrera).
+`;
+
+// Consult program, query goal, and show answers
+session.consult("TrafficControl.pl", {
+    success: function() {
+        session.query(goal, {
+            success: function() {
+                session.answers(show);
+            }
+        })
+    }
+});
